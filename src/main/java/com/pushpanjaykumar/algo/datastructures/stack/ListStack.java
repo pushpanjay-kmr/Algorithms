8db9302b43de1410c1a288891341c5436eaf2f25
pushpanjay.kumar <pushpanjay.kumar@yatra.com>
2020-09-17 11:12:53 +0530

    add stack implementation

--- src/main/java/com/pushpanjaykumar/algo/datastructures/stack/ListStack.java
+++ src/main/java/com/pushpanjaykumar/algo/datastructures/stack/ListStack.java
@@ -1,7 +1,56 @@
 package java.com.pushpanjaykumar.algo.datastructures.stack;
 
+import java.util.EmptyStackException;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 /**
  * @author pushpanjay.kumar
  */
-public class ListStack {
+public class ListStack<T> implements Iterable<T> ,Stack<T>{
+    //Linkedlist internally uses doubly linked list
+    private LinkedList<T> list = new LinkedList<>();
+
+    public ListStack() {
+    }
+
+    public ListStack(T firstElem) {
+        push(firstElem);
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size()==0;
+    }
+
+    @Override
+    public void push(T elem) {
+        list.addLast(elem);
+    }
+
+    @Override
+    public T pop() {
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        return list.removeLast();
+    }
+
+    @Override
+    public T peek() {
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        return list.peekLast();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return list.iterator();
+    }
 }
--- src/main/java/com/pushpanjaykumar/algo/datastructures/stack/Stack.java
+++ src/main/java/com/pushpanjaykumar/algo/datastructures/stack/Stack.java
@@ -3,5 +3,14 @@ package java.com.pushpanjaykumar.algo.datastructures.stack;
 /**
  * @author pushpanjay.kumar
  */
-public interface Stack {
+public interface Stack<T> {
+    int size();
+
+    boolean isEmpty();
+
+    void push(T elem);
+
+    T pop();
+
+    T peek();
 }
